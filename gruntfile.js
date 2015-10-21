184c102f467aba98699bb852826a14e18f32b620
Vinicius Nascimento <vinicius.nascimento05@gmail.com>
2015-10-21 13:17:15 -0300

    adicionado plugin imagemin no grunt

--- gruntfile.js
+++ gruntfile.js
@@ -48,6 +48,31 @@ module.exports = function(grunt){
 				src: ['dev/js/script.js']
 			}
 		},
+		imagemin: {
+			static: {
+				options: {
+					optimizationLevel: 3,
+					svgoPlugins: [{removeViewBox: false}]
+				},
+				files: {
+					'images/images/office.jpg': 'dev/images/office.jpg',
+					'images/images/VN icon.png': 'dev/images/VN icon.png',
+					'images/images/VN icon-black.png': 'dev/images/VN icon-black.png',
+					'images/images/VN logo2.png': 'dev/images/VN logo2.png',
+					'images/images/VN logo-fundo-branco2.png': 'dev/images/VN logo-fundo-branco2.png',
+					'images/images/VN logo-green-sea-fundo-branco.png': 'dev/images/VN logo-green-sea-fundo-branco.png',
+					'images/images/VN logo-green-sea-norma.png': 'dev/images/VN logo-green-sea-norma.png'
+				}
+				},
+				dynamic: {
+					files: [{
+						expand: true,
+						cwd: 'dev/images',
+						src: ['**/*.{png,jpg,gif}'],
+						dest: 'images/'
+					}]
+				}
+		},
 		watch: {
 			js: {
 				files: ['dev/js/*.js'],
@@ -77,5 +102,6 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 }
